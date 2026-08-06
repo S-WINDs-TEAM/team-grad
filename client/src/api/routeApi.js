f597fd3d620c3ffe0828104b18e6ef27a63927a1
MKFaroouq <iprobazooka@gmail.com>
2026-08-06 23:42:42 +0300

    modifiing getHistoryApi to request params

--- client/src/api/routeApi.js
+++ client/src/api/routeApi.js
@@ -1,6 +1,8 @@
 import axiosInstance from "./axiosInstance";
 const planRouteApi = (data) => axiosInstance.post('/routes/plan', data);
-const getHistoryApi = ()=> axiosInstance.get('/routes/history');
+const getHistoryApi = (page = 1 , limit = 10)=>
+     axiosInstance.get('/routes/history', 
+        { params: { page, limit } });
 
 export {
     planRouteApi,
